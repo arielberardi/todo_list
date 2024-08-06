1f7ede6f22989eb33aa47d53efa6349084e6d5ab
Ariel Berardi <ariel.berardi95@gmail.com>
2024-08-06 19:45:34 +0100

    Add broadcast stream for updates

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :status, presence: true, inclusion: { in: Task.statuses.keys }
 
+  broadcasts_refreshes_to ->(tasks) { 'tasks' }
+
   def to_s
     title
   end
